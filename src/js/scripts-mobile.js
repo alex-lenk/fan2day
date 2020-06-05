5ff9d166a48b5501db4618dcddb381a5df6397fa
alexlenk <alexlenk.ru@gmail.com>
2020-06-05 22:29:59 +0300

    Finish catalog on mobile

--- src/js/scripts-mobile.js
+++ src/js/scripts-mobile.js
@@ -37,9 +37,59 @@ $(document).ready(function () {
   });
   /* END */
 
+
+  /* BEGIN: Легкий аккордион применяется для сокращения огромных простыней с текстом */
   $('.accordion-light__head').click(function () {
     $(this).toggleClass('accordion-light__active').next().slideToggle();
   });
+  /* END */
+
+
+  /* BEGIN: Клик по кнопке "выбрать развлечения", появится панель с навигацией */
+  $('.nav-catalog-dropdown__close').click(function () {
+    $(this).closest('.js__tab-box').removeClass('active');
+  });
+  /* END */
+
+
+  /* BEGIN: Для панелей с фильтрами */
+  $('.catalog-widget__title').on('click', function () {
+    $(this).toggleClass('catalog-widget__active');
+    $(this).next().slideToggle();
+  });
+
+  $('.catalog-widget__clear').on('click', function () {
+    $(this).closest('.catalog-widget__body').find('.custom-control-input').prop('checked', false);
+
+    let countChecked = $('.catalog-filter').find('.custom-control-input:checked').length;
+
+    if (countChecked < 1) {
+      $(catalogFilter).removeClass('catalog-filter__show');
+    }
+  });
+
+  let catalogFilter = '.catalog-filter';
+  $('.catalog-widget__group').on('click', function () {
+    if ($(".catalog-widget__group .custom-control-input").is(":checked")) {
+      $(catalogFilter).addClass('catalog-filter__show');
+    } else {
+      $(catalogFilter).removeClass('catalog-filter__show');
+    }
+  });
+
+  $('.catalog-filter__close').click(function () {
+    $('.catalog-filter').removeClass('catalog-filter__opened');
+  });
+
+  $('.filter-panel__choose').click(function () {
+    $('.catalog-filter').addClass('catalog-filter__opened');
+  });
+
+  $('.catalog-filter__clear-all').click(function () {
+    $('.catalog-filter .custom-control-input').prop('checked', false);
+    $('.catalog-filter').removeClass('catalog-filter__show');
+  });
+  /* END */
 });
 
 
